7b4afca7092c238d9bb09bc8579358fb303c0765
Predrag Vila <predorama@gmail.com>
2021-12-25 21:19:41 +0500

    Created functionality for Register and Login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { Fragment } from 'react';
-import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
-const App = () => {
-  return (
-    <Fragment>
+const App = () => (
+  <Fragment>
+    <Router>
       <Navbar />
-      <Landing />
-    </Fragment>
-  );
-};
+      <section className='contaner'>
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route exact path='register' element={<Register />} />
+          <Route exact path='login' element={<Login />} />
+        </Routes>
+      </section>
+    </Router>
+  </Fragment>
+);
 
 export default App;
